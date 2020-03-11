6366665d2b622c4160bbb617fd675d8d37b6e71e
kevin.li <kevin.li@duethealth.com>
2020-03-11 13:13:10 -0400

    Successfully added a shim framework which can be used in respective targets

---
+++ RxSwift-Testing/RxShim/RxShim.h
@@ -0,0 +1,19 @@
+//
+//  RxShim.h
+//  RxShim
+//
+//  Created by Kevin Li on 3/11/20.
+//  Copyright © 2020 kevinli. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RxShim.
+FOUNDATION_EXPORT double RxShimVersionNumber;
+
+//! Project version string for RxShim.
+FOUNDATION_EXPORT const unsigned char RxShimVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxShim/PublicHeader.h>
+
+
